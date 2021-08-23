7dbf8befcc6cd165e704821f499a1783cae199e1
chris_render <chris_render@MacBook-Pro-de-Christian.local>
2021-08-23 00:31:01 -0300

    feat: add userModule && user dto && user controller

---
+++ react-test/src/User/user.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller()
+export class UserController {
+  constructor(private readonly userService: UserService) { }
+
+  @Get()
+  getHello(): string {
+    return this.userService.getHello();
+  }
+  @Post()
+  get(): string {
+    return this.userService.getHello();
+  }
+}
